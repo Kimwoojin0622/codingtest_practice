7052df4d4b84ac007db26497b39ec9cc42044dd0
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-10 00:45:21 +0900

    [level 0] Title: 수 조작하기 2, Time: 12.00 ms, Memory: 15.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,14 @@
+def solution(numLog):
+    result = []
+    
+    for i in range(1, len(numLog)):
+        if numLog[i-1] + 1 == numLog[i]:
+            result.append('w')
+        elif numLog[i-1] - 1 == numLog[i]:
+            result.append('s')
+        elif numLog[i-1] + 10 == numLog[i]:
+            result.append('d')
+        else:
+            result.append('a')
+    
+    return "".join(result)
