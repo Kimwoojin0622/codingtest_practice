80f130a6273b55c0252a50cfc374635ea383bb8e
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-09 23:25:18 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,7 @@
+def solution(todo_list, finished):
+    result = []
+    for i in range(len(finished)):
+        if finished[i] == False:
+            result.append(todo_list[i])
+    
+    return result
