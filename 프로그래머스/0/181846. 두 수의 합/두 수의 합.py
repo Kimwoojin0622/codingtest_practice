b3ae2e2a7f7ff785a32210cc90fef05c88d07dc9
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-17 00:55:27 +0900

    [level 0] Title: 두 수의 합, Time: 36.42 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.py
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.py
@@ -1,2 +1,33 @@
+def divide_i(i, tmp, length):
+    if i == (length - 1):
+        return str(tmp)
+    else:
+        return str(tmp % 10)
+
 def solution(a, b):
-    return str(int(a) + int(b))
+    a, b = a[::-1], b[::-1]
+
+    # a와 b중에 길이가 긴 것을 가져온다. 1234 + 56을 할 때, 1234 만큼 돌아야한다.
+    length = max(len(a), len(b))
+    result = []
+    tmp, remain = 0, 0
+    
+    for i in range(length):
+        if i > len(b) - 1:
+            tmp = int(a[i]) + 0 + remain
+            return_value = divide_i(i, tmp, length)
+            result.append(return_value)
+            remain = tmp // 10
+        elif i > len(a) - 1:
+            tmp = 0 + int(b[i]) + remain
+            return_value = divide_i(i, tmp, length)
+            result.append(return_value)
+            remain = tmp // 10
+        else:
+            tmp = int(a[i]) + int(b[i]) + remain
+            return_value = divide_i(i, tmp, length)
+            result.append(return_value)
+            remain = tmp // 10
+    # 거꾸로된 result를 원래 상태로 만든다.
+    result = result[::-1]
+    return "".join(result)
