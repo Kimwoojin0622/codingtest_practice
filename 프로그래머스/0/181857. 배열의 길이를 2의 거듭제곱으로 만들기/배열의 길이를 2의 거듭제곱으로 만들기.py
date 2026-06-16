27224261e1f94b7619dd3fe83ff2a139bcb0e6c9
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-16 13:42:36 +0900

    Update 배열의 길이를 2의 거듭제곱으로 만들기.py

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -1,7 +1,9 @@
 def solution(arr):
-    sq_2 = [2**i for i in range(1001)] # O(N) 1000
+    # print(2 ** 10)
+    # arr의 길이가 최대 1000이기 때문에, 1000이 넘는 2의 거듭제곱 => 2 ** 10 == 1024까지 돌린다.
+    sq_2 = [2**i for i in range(11)] # O(10)
     temp = 0
-    for n in sq_2: # O(N)
+    for n in sq_2:
         if len(arr) <= n: # O(1)
             temp = n
             break
