041d6653c6b375f81aae0cc82c09b37a40c8fcaa
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-16 13:33:02 +0900

    Update 무작위로 K개의 수 뽑기.py

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -7,6 +7,7 @@ def solution(arr, k):
             break
     
     if len(result) < k:
+        # 완성된 배열의 길이가 k보다 작을 시, result 리스트에 -1 원소 추가
         length = k - len(result)
         for _ in range(length):
             result.append(-1)
