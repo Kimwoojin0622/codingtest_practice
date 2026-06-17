95e43d3d86e038f95112f4071949cf95f377514c
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-17 13:32:59 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,12 +1,9 @@
 def solution(date1, date2):
-    if date1[0] <= date2[0]:
-        if date1[0] < date2[0]:
+    for date1_ymd, date2_ymd in zip(date1, date2):
+        if date1_ymd < date2_ymd:
             return 1
+        elif date1_ymd > date2_ymd:
+            return 0
         else:
-            if date1[1] <= date2[1]:
-                if date1[1] < date2[1]:
-                    return 1
-                else:
-                    if date1[2] < date2[2]:
-                        return 1
+            continue
     return 0
