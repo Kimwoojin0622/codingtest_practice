e1978c43c3460b40c2658df1f3a3fbbe146299d7
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-18 17:29:45 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.31 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,30 +1,41 @@
 def solution(n):
+    # 0이 담긴 n x n 배열 생성
     result = [[0] * n for _ in range(n)]
+    
+    # 들렸던 곳인지 체크할 temp 생성
     temp = set()
     
-    # 움직이는 방향
     # 우 하 좌 상
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
     
-    # 움직일 x, y 변수와 result[x][y]에 들어갈 값 num 선언
-    x, y, num = 0, 0, 1
-    # 움직일 방향 di 선언
+    # 움직일 말 위치 x, y 선언
+    x, y = 0, 0
+    
+    # 우 하 좌 상 방향을 선택할 di 선언
     di = 0
     
-    while num <= (n ** 2):
-        result[x][y] = num
+    # element = 1 부터 시작해 n ** 2까지 값을 계속 집어넣는다.
+    element = 1
+    
+    # element가 n ** 2 이전까지 실행, 왜냐하면 n ** 2까지의 숫자를 담아야함
+    while element <= (n ** 2):
+        result[x][y] = element
         temp.add((x,y))
         
+        # 다음 위치 계산
+        # ex) 원래 0,0 이면 -> 0,1로 변함
         nx = x + dx[di]
         ny = y + dy[di]
         
-        if nx < 0 or nx > (n - 1) or ny < 0 or ny > (n - 1) or (nx, ny) in temp:
+        # 다음 위치가 해당 n x n 배열을 넘어가거나, 이미 들렸던 곳이면 방향을 바꿔야함
+        if nx < 0 or nx > (n-1) or ny < 0 or ny > (n-1) or (nx, ny) in temp:
+            # 우 하 좌 상 방향으로 만들어진 dx, dy를 계속 돌아야함
             di = (di + 1) % 4
             nx = x + dx[di]
             ny = y + dy[di]
         
         x, y = nx, ny
-        num += 1
-    
+        element += 1
+
     return result
