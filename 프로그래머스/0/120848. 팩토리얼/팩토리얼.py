e03b92254c0147159e4dc91ecaca95d7f1a7ab4c
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-25 13:38:33 +0900

    Update 팩토리얼.py

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,11 +1,11 @@
 def solution(n):
-    temp, i, is_True = 1, 1, True
+    temp, i = 1, 1
     
-    while is_True:
+    while True:
         temp = temp * i
         if temp > n:
             result = i - 1
-            is_True = False
+            break
         i = i + 1
     
     return result
