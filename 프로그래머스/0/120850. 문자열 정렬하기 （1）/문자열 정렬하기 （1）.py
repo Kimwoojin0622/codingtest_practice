40c3463b55a6631080331df6ab8866181e8d4f78
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-25 13:45:30 +0900

    Update 문자열 정렬하기 （1）.py

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,10 +1,5 @@
 def solution(my_string):
-    chk_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    result = []
-    for s in my_string:
-        if s in chk_num:
-            num = int(s)
-            result.append(num)
+    result = [int(s) for s in my_string if s.isdigit()]
+    result.sort()
     
-    result.sort() # O(N log N)
     return result
