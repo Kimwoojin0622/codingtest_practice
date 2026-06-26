1ea4bc9cb07231161a029790a6b6ced6564caf2a
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-06-27 02:14:06 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,17 @@
+def solution(numbers):
+    result = ''
+    numbers_dict = {
+        'zero' : '0', 'one' : '1', 'two' : '2', 'three' : '3', 'four' : '4', 'five' : '5',
+        'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9',
+    }
+    
+    tmp = ''
+    for s in numbers:
+        tmp = tmp + s
+        if tmp in numbers_dict:
+            result = result + numbers_dict[tmp]
+            tmp = ''
+            
+    return int(result)
+            
+    
