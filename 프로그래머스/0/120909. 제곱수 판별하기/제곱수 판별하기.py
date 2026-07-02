b6a8e10133c6d785a20086f9fb92dd0ac811dc9f
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-07-03 02:35:33 +0900

    Update 제곱수 판별하기.py

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,10 +1,9 @@
+import math
+
 def solution(n):
-    tmp = n // 2
-    
+    tmp = int(math.sqrt(n)) + 1
     for i in range(1, tmp + 1):
         if i * i == n:
             return 1
     
     return 2
-    
-    
