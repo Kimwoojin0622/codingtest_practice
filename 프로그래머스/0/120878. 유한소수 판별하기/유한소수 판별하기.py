d3b0eedb91b96b08a5b2d97243e4de4425169150
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-07-03 13:33:59 +0900

    Update 유한소수 판별하기.py

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -5,18 +5,13 @@ def solution(a, b):
     a, b = a // tmp, b // tmp # 최대공약수 사용해서 기약분수 구하기
     
     # 1 제외, 분모의 약수 구하기
-    tmp_list = []
-    for i in range(2, b + 1):
-        if b % i == 0:
-            tmp_list.append(i)
-    
-    cnt = 0
+    tmp_list = [i for i in range(2, b + 1) if b % i == 0]
+
+    cnt = 0 # 유한소수, 무한소수 판별
     for data in tmp_list:
         if data % 2 == 0 or data % 5 == 0:
             continue
         else:
-            cnt += 1
-            if cnt >= 1:
-                return 2
-    
+            return 2
+            
     return 1
