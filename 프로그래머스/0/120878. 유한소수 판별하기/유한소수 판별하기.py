47c6055b18069a1e556435614ee0c1ad02da6692
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-07-03 13:30:24 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,22 @@
+import math
+
+def solution(a, b):
+    tmp = math.gcd(a,b)
+    a, b = a // tmp, b // tmp # 최대공약수 사용해서 기약분수 구하기
+    
+    tmp_list = []
+    for i in range(2, b + 1):
+        if b % i == 0:
+            tmp_list.append(i)
+    
+    cnt = 0
+    for data in tmp_list:
+        if data % 2 == 0 or data % 5 == 0:
+            continue
+        else:
+            cnt += 1
+
+    if cnt >= 1:
+        return 2
+    else:
+        return 1
