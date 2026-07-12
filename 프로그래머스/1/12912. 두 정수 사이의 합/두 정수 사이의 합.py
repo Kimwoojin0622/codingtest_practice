ca78709ead208e000f273231b727c8be99839e3d
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-07-12 22:28:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 448.94 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    if a == b:
+        return a
+    else:
+        result = 0
+        for i in range(min(a,b), max(a, b) + 1):
+            result += i
+    
+    return result
