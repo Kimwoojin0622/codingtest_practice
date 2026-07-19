2d404f22336231dcd8439809b130ec6c25d085f2
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-07-19 15:09:34 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    i, chk = 0, 0
+    result = []
+    while i < len(s):
+        if s[i] == ' ':
+            result.append(s[i])
+            chk = 0
+        elif chk % 2 == 0:
+            result.append(s[i].upper())
+            chk += 1
+        elif chk % 2 != 0:
+            result.append(s[i].lower())
+            chk += 1
+        i += 1
+    
+    return "".join(result)
+        
