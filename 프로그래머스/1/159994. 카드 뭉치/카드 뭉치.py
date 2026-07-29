65b523e45f9db30e9a2a8f1e3fc1ccc5840ea571
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-07-29 14:09:58 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,16 +1,13 @@
+from collections import deque
 def solution(cards1, cards2, goal): 
-    cards1 = cards1 + ['99']
-    cards2 = cards2 + ['99']
+    cards1 = deque(cards1 + ['99'])
+    cards2 = deque(cards2 + ['99'])
     
-    i, j = 0, 0
     for data in goal:
-        if data == cards1[i]:
-            t = cards1[i]
-            i += 1
-        elif data == cards2[j]:
-            t = cards2[j]
-            j += 1
-        elif data != cards1[i] or data != cards2[j]:
+        if data == cards1[0]:
+            cards1.popleft()
+        elif data == cards2[0]:
+            cards2.popleft()
+        elif data != cards1[0] or data != cards2[0]:
             return "No"
     return "Yes"
-                
