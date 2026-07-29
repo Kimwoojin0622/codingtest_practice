4a6b156c82f6de2be408a96a2292c3f92fbf184e
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-07-29 13:42:17 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.26 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,20 @@
+def solution(k, score):
+    # O(N^2) 가능
+    result = [] # 최솟값 리스트
+    tmp = [] # 명예의 전당 리스트
+    for i in range(len(score)):
+        if len(tmp) < k:
+            tmp.append(score[i])
+            tmp.sort(reverse=True) # O(n log n)
+            result.append(tmp[-1])
+        else:
+            if score[i] > tmp[-1]:
+                tmp[-1] = score[i]
+                tmp.sort(reverse=True)
+                result.append(tmp[-1])
+            else:
+                result.append(tmp[-1])
+
+    return result
+        
+        
