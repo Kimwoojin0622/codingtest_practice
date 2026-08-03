eb11e123c7ef0fa7ec34b784792ac92bc404a95b
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-08-03 14:31:31 +0900

    [level 1] Title: 기사단원의 무기, Time: 79.07 ms, Memory: 12.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,18 @@
+import math
+def solution(number, limit, power):
+    # O(N^2)은 무리
+    knight = [0] * number
+    for i in range(1, number + 1):
+        for j in range(i, number + 1, i):
+            knight[j-1] += 1
+        
+    result = 0
+    for k in knight:
+        if k <= limit:
+            result += k
+        else:
+            result += power
+    
+    return result
+            
+        
