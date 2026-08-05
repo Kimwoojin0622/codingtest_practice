afd20898e9c43f53db4aef7736d46a8c622180d3
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-08-05 14:33:04 +0900

    Time: 475 ms (5%), Space: 0B (100%) - LeetHub

--- LeetCode/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ LeetCode/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -7,21 +7,3 @@ FROM (SELECT MACHINE_ID
       FROM ACTIVITY
       GROUP BY MACHINE_ID, PROCESS_ID) AS DIFF_TABLE
 GROUP BY MACHINE_ID;
-
-
-
--- SELECT MACHINE_ID
---      , PROCESS_ID
---      , MAX(TIMESTAMP) - MIN(TIMESTAMP) AS 'DIFF'
---      , COUNT(*) AS 'CNT'
--- FROM ACTIVITY
--- GROUP BY MACHINE_ID, PROCESS_ID
-
--- | machine_id | processing_time |
--- | ---------- | --------------- |
--- | 4          | 62.61           |
--- | 0          | 4.208           |
--- | 1          | 24.028          |
--- | 3          | 50.399          |
--- | 2          | 44.165          |
--- | 5          | 31.642          |
