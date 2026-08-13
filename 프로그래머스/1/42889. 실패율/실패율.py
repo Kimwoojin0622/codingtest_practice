c7dc198c09b62faef3130e55f45e1921ee0a8f96
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-08-13 14:18:41 +0900

    [level 1] Title: 실패율, Time: 3678.16 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -3,19 +3,26 @@ def solution(N, stages):
     # N 최대 500 , stage길이 최대 200,000
     
     tmp = []
+    # step 1. 각각의 실패율을 구하자
+    # step 2. N을 1부터 N+1까지 i로 돌린다.
     for i in range(1, N + 1):
+        # step 3. i의 count를 세고, stages를 for문으로 돌면서 i 보다 크거나 같은 숫자를 센다.
         lose_player_cnt = stages.count(i) # 실패한 유저
         player_cnt = 0
         for num in stages: # 해당 스테이지 플레이한 유저
             if num >= i:
                 player_cnt += 1
-                
+        
+        # step 4. 만약 lose_player가 한 명도 없다면 실패율 0을 append
         if lose_player_cnt == 0:
             tmp.append([i, 0.0])
+        # step 5. i의 count / i보다 크거나 같은 숫자 = lose_player_cnt / player_cnt
+        # step 6. 실패율을 집어넣는다.
         else:
             tmp.append([i, lose_player_cnt / player_cnt])
-
+            
+    # step 7. 정렬
     sort_result = sorted(tmp, key=lambda x:(-x[1], x[0]))
-    
+    # step 8. 결과를 result 리스트에 삽입
     result = [data[0] for data in sort_result]
     return result
