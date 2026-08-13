226cfa4504faa867ed7ba0af86d2de3837fcc818
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-08-13 13:40:46 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,27 @@
+def solution(s):
+    # O(N^2) 가능
+    result = []
+    num = 0
+    for st in s:
+        if st == " ":
+            num = 0
+            result.append(st)
+            continue
+        
+        if st.isdigit():
+            num += 1
+            result.append(st)
+            continue
+        elif num == 0:
+            st = st.upper()
+            result.append(st)
+            num += 1
+        else:
+            st = st.lower()
+            result.append(st)
+            num += 1
+    
+    answer = "".join(result)
+    return answer
+        
+        
