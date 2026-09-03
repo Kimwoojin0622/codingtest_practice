0affe52c12830cd6822fce0674825efaae675695
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-09-03 20:33:01 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.61 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,25 @@
+def solution(keymap, targets):
+    key_location = {}
+    for key in keymap: # 100 * 100
+        for k in key:
+            if k not in key_location:
+                key_location[k] = key.index(k) + 1
+            elif k in key_location:
+                if key.index(k) + 1 < key_location[k]:
+                    key_location[k] = key.index(k) + 1
+    
+    result = [0] * len(targets)
+    
+    i = 0
+    for target in targets: # 100 * 100
+        target_sum = 0
+        for t in target:
+            if t not in key_location:
+                target_sum = -1
+                break
+            else:
+                target_sum += key_location[t]
+        result[i] = target_sum
+        i += 1
+
+    return result
