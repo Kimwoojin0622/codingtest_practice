a75e7b2358dd5f2d451eb61ec0405e3868337b51
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-09-07 15:33:52 +0900

    [level 2] Title: 할인 행사, Time: 98.84 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,19 +1,14 @@
+from collections import Counter
 def solution(want, number, discount):
     # 10 * 100,000
     want_number = {}
     for i in range(len(want)):
         want_number[want[i]] = number[i]
     
-    loop = len(discount) - 9
     result = 0
-    for i in range(loop):
-        count = 0
-        today_discount = discount[i:i+10]
-        for j in range(len(want)):
-            if today_discount.count(want[j]) >= want_number[want[j]]:
-                count += 1
-                continue
-        if count == len(want):
+    for i in range(len(discount) - 9):
+        chk = Counter(discount[i:i+10])
+        if chk == want_number:
             result += 1
     
     return result
