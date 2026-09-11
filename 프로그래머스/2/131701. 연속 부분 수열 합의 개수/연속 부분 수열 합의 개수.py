72d562b081c9ac6f02b7397928810fa91fe64fb2
김우진 <101042244+Kimwoojin0622@users.noreply.github.com>
2026-09-11 13:07:50 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 129.51 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,13 +1,12 @@
 def solution(elements):
-    join = elements + elements
-    sums = set()
-    
+    double = elements + elements
     length = len(elements)
+    tmp = set()
     
     for i in range(length):
         total = 0
-        for j in range(i, length + i):
-            total += join[j]
-            sums.add(total)
+        for j in range(i, length+i):
+            total += double[j]
+            tmp.add(total)
     
-    return len(sums)
+    return len(tmp)
